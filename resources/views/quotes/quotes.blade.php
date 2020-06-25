a32822ee7d65a19585cea069bfc34a2dd4791c21
Robert Hernandez <rahernande96@gmail.com>
2020-06-25 15:03:11 -0400

    fix jquery

--- resources/views/quotes/quotes.blade.php
+++ resources/views/quotes/quotes.blade.php
@@ -282,7 +282,7 @@
 
     <script
       type="text/javascript"
-      src="http://code.jquery.com/jquery-1.11.3.min.js"
+      src="https://code.jquery.com/jquery-1.11.3.min.js"
     ></script>
     <!--jQuery-->
     <script
