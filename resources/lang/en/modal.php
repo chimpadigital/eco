6a2e259afa55ebe0f72188ccdf4b661427b0b8f4
Robert Hernandez <rahernande96@gmail.com>
2020-07-01 19:42:33 -0400

    modal translation

---
+++ resources/lang/en/modal.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'title_1' => 'Remember',
+    'p_1' => 'You can abandon and resume the manual download process at any time from any device by entering the email provided in the first form',
+    'btn' => 'Log in',
+];
---
+++ resources/lang/es/modal.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'title_1'=>'Recuerde',
+    'p_1'=>'Podrá abandonar y retomar el proceso de descarga del manual en cualquier momento desde cualquier dispositivo ingresando el correo electrónico provisto en el primer formulario',
+    'btn'=>'Ingrese aqui',
+];
