f487eb7fe0acc7f5792efe1fb0030dbada5f8f89
chimpadigital <sdesigncba@gmail.com>
2020-07-02 16:09:49 -0300

    ajusetes menores

--- resources/views/admins/auth/verify.blade.php
+++ resources/views/admins/auth/verify.blade.php
@@ -5,20 +5,20 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
+                <div class="card-header">{{ __('Verifique su casilla de mail') }}</div>
 
                 <div class="card-body">
                     @if (session('resent'))
                         <div class="alert alert-success" role="alert">
-                            {{ __('A fresh verification link has been sent to your email address.') }}
+                            {{ __('Se ah enviado un nuevo link de verificación de contraseña.') }}
                         </div>
                     @endif
 
-                    {{ __('Before proceeding, please check your email for a verification link.') }}
-                    {{ __('If you did not receive the email') }},
+                    {{ __('Antes de continuar, proceda con el link que se envió a su mail.') }}
+                    {{ __('Si no recibió el mail') }},
                     <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
                         @csrf
-                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
+                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('Click aquí para reenviarlo') }}</button>.
                     </form>
                 </div>
             </div>
